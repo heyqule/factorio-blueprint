4b9ee94b86eb3cc8e5ab18fe8150cc3ffea189a3
heyqule <heyqule@users.noreply.github.com>
2017-07-01 22:43:51 -0400

    Update deleteChunk.lua
    Remove count print

--- deleteChunk.lua
+++ deleteChunk.lua
@@ -3,7 +3,6 @@ local surface = game.player.surface
 for c in surface.get_chunks() do     
 	local canRemove = true
 	local count = surface.count_entities_filtered({area={{c.x * 32 - 4, c.y * 32 - 4}, {c.x * 32 + 36, c.y * 32 + 36}},force="player"});
-	game.player.print(count);
 	if(count > 0) then
 		canRemove = false
 	end   	
